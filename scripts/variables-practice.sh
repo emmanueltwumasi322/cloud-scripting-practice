8397d146e69e10fd7e2e466a3014b4304b1a6015
Emmanuel Twumasi <Twumasi.emmanuel322@gmail.com>
2026-09-25 11:37:38 -0400

    Add Day 9 Bash variables practice

---
+++ scripts/variables-practice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SERVER="web-server-01"
+REGION="us-east-1"
+ENVIRONMENT="dev"
+OWNER="Manny"
+SERVICE="EC2"
+
+echo "Server name: $SERVER"
+echo "Region: $REGION"
+echo "Environment: $ENVIRONMENT"
+echo "$OWNER is checking $SERVICE in $REGION"
